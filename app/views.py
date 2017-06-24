3879e8193a01b7b659ae137a04f8cb5b0f5a90a5
lowlp86 <lowlp86@gmail.com>
2017-06-25 00:04:43 +0800

    fuck

--- app/views.py
+++ app/views.py
@@ -25,6 +25,11 @@ def index():
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+	flash('Login requested for Name : ' + form.name.data)
+        flash('passwde: ' + str(form.password.data))
+        flash('remember_me: ' + str(form.remember_me.data))
+	return redirect('/index')
     return render_template('login.html',
-        title = "Sign In",
-        form = form)
+        		title = "Sign In",
+		        form = form)
