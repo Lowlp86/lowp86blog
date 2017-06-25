662c364ff4e09845bc22cef8bdac696482145872
lowlp86 <lowlp86@gmail.com>
2017-06-25 09:43:10 +0800

    new database

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-from app import views
+from app import views, models
---
+++ db_downgrade.py
@@ -0,0 +1,8 @@
+from migrate.versioning import api
+from config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_MIGRATE_REPO
+
+v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REpO)
+api.downgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, v - 1)
+print 'Current database ver : ' + str(api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO))
+
